0117b4cb935b41bebfba89aa31668dd42c726146
Curtis Rubeck <rubeckcurtis@gmail.com>
2023-12-29 09:28:49 -0600

    feat: init lua config

---
+++ lua/options.lua
@@ -0,0 +1,14 @@
+-- :help options
+vim.opt.modified = true
+vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.opt.expandtab = true                        -- convert tabs to spaces
+vim.opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4                             -- insert 4 spaces for a tab
+vim.opt.number = true                           -- set numbered lines
+vim.opt.cursorline = true                       -- highlight the current line
+vim.opt.relativenumber = false                  -- set relative numbered lines
+vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
+vim.opt.ignorecase = true                       -- ignore case in search patterns
+vim.opt.termguicolors = true
+
+vim.g.mapleader = " "
