02f0bc671f57f2a9504e39ec551c68e8e44acf27
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-05-02 15:12:40 -0500

    feat: auto install treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
+            auto_install = true,
             ensure_installed = { "lua", "javascript", "html", "python" },
             highlight = { enable = true },
             indent = { enable = true },
