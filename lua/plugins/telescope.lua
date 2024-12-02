4645b2b43901a0191dc895f8ef89791e7887bde4
Curtis Rubeck <curt@Curtiss-Laptop.local>
2024-12-02 10:58:00 -0600

    feat: use sf instead of pf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
         -- Add the configuration to ignore node_modules
         require('telescope').setup{
