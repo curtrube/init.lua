e006b6e8bc5ba4f1bfdecc0c65a9247801962cf4
Curtis Rubeck <curt@Curtiss-Laptop.local>
2024-12-10 19:58:49 -0600

    feat: add after and other stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,18 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            auto_install = true,
             ensure_installed = { "lua", "javascript", "html", "python" },
-            highlight = { enable = true },
+            auto_install = true,
+            highlight = {
+              enable = true,
+              disable = function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+              end,
+            },
             indent = { enable = true },
         })
     end,
