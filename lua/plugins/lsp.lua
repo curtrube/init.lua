935d854e716213c2458a1820c7112276c7148bde
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-12-19 20:12:01 -0600

    feat: add terraform lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pylsp", "ruff_lsp", "tsserver" },
+                ensure_installed = { "lua_ls", "pylsp", "ruff_lsp", "tsserver", "terraformls" },
             })
         end,
     },
@@ -34,6 +34,7 @@ return {
             lspconfig.pylsp.setup({})
             lspconfig.ruff_lsp.setup({})
             lspconfig.tsserver.setup({})
+            lspconfig.terraformls.setup({})
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
