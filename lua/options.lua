c6bee57b943aa7ac7c8d3e913c5bfb6a8e2a1cc0
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-12-31 23:08:00 -0600

    feat: add a bunch of options

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,4 @@
+-- [[ Setting options ]]
 -- :help options
 vim.opt.modified = true
 vim.opt.fileencoding = "utf-8" -- the encoding written to a file
@@ -8,8 +9,24 @@ vim.opt.tabstop = 4            -- insert 4 spaces for a tab
 vim.opt.softtabstop = 4
 
 vim.opt.number = true             -- set numbered lines
+vim.opt.relativenumber = true     -- set relative numbered lines
 vim.opt.cursorline = true         -- highlight the current line
-vim.opt.relativenumber = false    -- set relative numbered lines
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.ignorecase = true         -- ignore case in search patterns
 vim.opt.termguicolors = true
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
