82de6948afe237ccb4e36bd9dd0a30819b0a430b
Curtis Rubeck <rubeckcurtis@gmail.com>
2025-06-17 09:28:20 -0500

    formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,68 +1,70 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"pylsp",
-					"terraformls",
-					"gopls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"saghen/blink.cmp",
-			{
-				"folke/lazydev.nvim",
-				ft = "lua", -- only load on lua files
-				opts = {
-					library = {
-						-- See the configuration section for more details
-						-- Load luvit types when the `vim.uv` word is found
-						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-					},
-				},
-			},
-		},
-		config = function()
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
-			local lspconfig = require("lspconfig")
-			local util = require("lspconfig/util")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.pylsp.setup({})
-			lspconfig.terraformls.setup({})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-				cmd = { "gopls" },
-				filetypes = { "go", "gomod", "gowork", "gotmpl" },
-				root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "LspAttach",
-		opts = {
-			bind = true,
-			hint_enable = false,
-			floating_window = true,
-			toggle_key = "<C-s>", -- toggle floating window
-		},
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pylsp",
+          "terraformls",
+          "gopls",
+          "yamlls",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "saghen/blink.cmp",
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      local lspconfig = require("lspconfig")
+      local util = require("lspconfig/util")
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.pylsp.setup({})
+      lspconfig.terraformls.setup({})
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        cmd = { "gopls" },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+      })
+      lspconfig.yamlls.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "LspAttach",
+    opts = {
+      bind = true,
+      hint_enable = false,
+      floating_window = true,
+      toggle_key = "<C-s>", -- toggle floating window
+    },
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,20 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        -- Add the configuration to ignore node_modules
-        require('telescope').setup{
-            defaults = {
-                file_ignore_patterns = {
-                    "node_modules/*",   -- Ignore node_modules directory
-                    "%.git/*",           -- Ignore .git folders
-                },
-            },
-        }
-    end,
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.6",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader><space>", builtin.buffers, {})
+    -- Add the configuration to ignore node_modules
+    require("telescope").setup({
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules/*", -- Ignore node_modules directory
+          "%.git/*",   -- Ignore .git folders
+        },
+      },
+    })
+  end,
 }
