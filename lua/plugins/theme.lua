f73661485b607b49a913c1e4e4f952cf1a3f0be4
Curtis Rubeck <rubeckcurtis@gmail.com>
2025-08-22 13:09:46 -0500

    gruvbox theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -17,7 +17,18 @@ return {
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.gruvbox_material_enable_italic = true
+			vim.cmd.colorscheme("gruvbox-material")
 		end,
 	},
 }
