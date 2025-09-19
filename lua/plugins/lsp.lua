be97be3c5ce844d146d66c305c1feec617956af5
Curtis Rubeck <rubeckcurtis@gmail.com>
2025-09-18 21:34:17 -0500

    theme and pyright

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -221,7 +221,7 @@ return {
 			local servers = {
 				-- clangd = {},
 				gopls = {},
-				-- pyright = {},
+				pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
